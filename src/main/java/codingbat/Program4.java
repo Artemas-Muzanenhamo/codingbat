a462f3b21befb1dc95b2fc9f6ed97547432ce435
amuzanenhamo-zaizi <amuzanenhamo@zaizi.com>
2017-01-25 18:43:46 +0000

    Completed Program 4 Using the TDD Approach.

---
+++ src/main/java/codingbat/Program4.java
@@ -0,0 +1,15 @@
+package codingbat;
+
+public class Program4 {
+	
+	public static boolean mixStart(String str){
+		
+		if ((str.length() > 2) && (str.contains("ix"))){
+			return true;
+		}
+		
+		return false;
+		
+	}
+
+}
---
+++ src/test/java/codingbat/Program4Test.java
@@ -0,0 +1,31 @@
+package codingbat;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+//Return true if the given string begins with "mix", except the 'm' can be anything, so "pix", "9ix" .. all count.
+//
+//mixStart("mix snacks") → true
+//mixStart("pix snacks") → true
+//mixStart("piz snacks") → false
+
+
+public class Program4Test {
+	
+	@Test
+	public void whenWordLengthIsLessThan2ThenReturnFalse(){
+		
+		assertFalse(Program4.mixStart("a"));
+		
+	}
+	
+	@Test
+	public void whenWordContainsIXThenReturnTrue(){
+		
+		assertTrue(Program4.mixStart("mix snacks"));
+		
+	}
+	
+}
