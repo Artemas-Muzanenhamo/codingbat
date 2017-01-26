c59131bb01b719248ba90925841ee869fd6f11a3
amuzanenhamo-zaizi <amuzanenhamo@zaizi.com>
2017-01-26 16:58:26 +0000

    everyNth - Program 11 :  Completed using the TDD approach.

---
+++ src/main/java/codingbat/Program11.java
@@ -0,0 +1,25 @@
+package codingbat;
+
+public class Program11 {
+	
+	public static String everyNth(String str, int n){
+		
+		String result = "";
+		
+		if (!str.isEmpty() && n >= 1){
+			if(str.length() < n){
+				return str.substring(0, 1);
+			}else{
+				// Look at every nth char
+				for (int i=0; i<str.length(); i = i + n) {
+					result = result + str.charAt(i);
+				}
+				
+				return result;
+			}
+		}else{
+			return str;
+		}
+	}
+	
+}
---
+++ src/test/java/codingbat/Program11Test.java
@@ -0,0 +1,38 @@
+package codingbat;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+//Given a non-empty string and an int N, return the string made starting with char 0, and then every Nth char of the string. 
+//So if N is 3, use char 0, 3, 6, ... and so on. N is 1 or more.
+//
+//everyNth("Miracle", 2) → "Mrce"
+//everyNth("abcdefg", 2) → "aceg"
+//everyNth("abcdefg", 3) → "adg"
+
+public class Program11Test {
+	
+	@Test
+	public void whenStringPassedIsEmptyThenReturnEmptyString(){
+		assertEquals("", Program11.everyNth("", 5));
+	}
+	
+	@Test
+	public void whenNumberIsLessThanOneThenReturnString(){
+		assertEquals("hello", Program11.everyNth("hello", 0));
+	}
+	
+	@Test
+	public void whenNumberIsGreaterThanStringLengthThenReturnStringAsIs(){
+		assertEquals("h", Program11.everyNth("hello", 6));
+	}
+	
+	@Test
+	public void whenNthCharOfStringIsPassedThenReturnNewString(){
+		assertEquals("Mrce", Program11.everyNth("Miracle", 2));
+		assertEquals("aceg", Program11.everyNth("abcdefg", 2));
+		assertEquals("adg", Program11.everyNth("abcdefg", 3));
+	}
+	
+}
