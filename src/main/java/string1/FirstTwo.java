bc05b0aff58651b9eccfcfa1b9f067f20bdf518f
amuzanenhamo-zaizi <amuzanenhamo@zaizi.com>
2017-04-24 10:11:21 +0100

    Completed FirstTwo using the TDD approach.

---
+++ src/main/java/string1/FirstTwo.java
@@ -0,0 +1,17 @@
+package string1;
+
+/**
+ * Created by amuzanenhamo on 24/04/2017.
+ */
+public class FirstTwo {
+    public static String firstTwo(String str) {
+
+        // If String str is not empty and it's length is greater than 1...
+        if (!str.isEmpty() && str.length() > 1){
+            // String str = the first 2 characters of the string
+            str = str.substring(0, 2);
+        }
+
+        return str;
+    }
+}
---
+++ src/test/java/string1/FirstTwoTest.java
@@ -0,0 +1,39 @@
+package string1;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by amuzanenhamo on 24/04/2017.
+ *
+ * Given a string, return the string made of its first two chars, so the String "Hello" yields "He".
+ * If the string is shorter than length 2, return whatever there is, so "X" yields "X", and the empty string "" yields the empty string "".
+ * Note that str.length() returns the length of a string.
+ *
+ * firstTwo("Hello") → "He"
+ * firstTwo("abcdefg") → "ab"
+ * firstTwo("ab") → "ab"
+ */
+public class FirstTwoTest {
+
+    @Test
+    public void whenEmptyStringIsPassedInThenReturnEmptyString(){
+        assertEquals("", FirstTwo.firstTwo(""));
+    }
+
+    @Test
+    public void whenStringPassedIsLength1ThenReturnStringAsIs(){
+        assertEquals("X", FirstTwo.firstTwo("X"));
+    }
+
+    @Test
+    public void whenStringPassedIsLength2ThenReturnTheTwoChars(){
+        assertEquals("ab", FirstTwo.firstTwo("ab"));
+    }
+
+    @Test
+    public void whenStringPassedHasLengthMoreThan3ThenReturnFirst2CharsOfTheString(){
+        assertEquals("Ar", FirstTwo.firstTwo("Artemas"));
+    }
+}
