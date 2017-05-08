5d6cbfbb8019069fdf661ab860ac0a4f6bf9c663
amuzanenhamo-zaizi <amuzanenhamo@zaizi.com>
2017-05-08 12:01:15 +0100

    Completed Sum3 using the TDD approach.

---
+++ src/main/java/array1/Sum3.java
@@ -0,0 +1,17 @@
+package array1;
+
+/**
+ * Created by amuzanenhamo on 08/05/2017.
+ */
+public class Sum3 {
+    public static int sum3(int[] nums) {
+
+        // if `nums` is not empty
+        if (nums.length != 0){
+            return nums[0] + nums[1] + nums [2];
+        }
+
+        return 0;
+
+    }
+}
---
+++ src/test/java/array1/Sum3Test.java
@@ -0,0 +1,33 @@
+package array1;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by amuzanenhamo on 08/05/2017.
+ *
+ * Given an array of ints length 3, return the sum of all the elements.
+ *
+ * sum3([1, 2, 3]) → 6
+ * sum3([5, 11, 2]) → 18
+ * sum3([7, 0, 0]) → 7
+ */
+public class Sum3Test {
+
+    int[] emptyArray = {};
+    int[] arrayCase1 = {1, 2, 3};
+    int[] arrayCase2 = {5, 11, 2};
+    int[] arrayCase3 = {7, 0, 0};
+
+    @Test
+    public void whenAnEmptyArrayIsPassedThenReturnZero(){
+        assertEquals(0, Sum3.sum3(emptyArray));
+    }
+
+    @Test
+    public void whenArrayOfLength3IsPassedThenReturnTheSumOfAllElementValues(){
+        assertEquals(6, Sum3.sum3(arrayCase1));
+    }
+
+}
