20dc24c7645b3d35bb9f28104ba95e5f6e9d8fd4
amuzanenhamo-zaizi <amuzanenhamo@zaizi.com>
2017-05-22 17:40:41 +0100

    added Hman to the Class for Copyright purposes

--- src/test/java/map1/MapBullyTest.java
+++ src/test/java/map1/MapBullyTest.java
@@ -10,6 +10,7 @@ import static org.junit.Assert.assertTrue;
 
 /**
  * Created by amuzanenhamo on 22/05/2017.
+ * Contributed by Hemanthi (c)
  *
  * Modify and return the given map as follows: if the key "a" has a value, set the key "b" to have that value, and set the key "a" to have the value "".
  * Basically "b" is a bully, taking the value and replacing it with the empty string
