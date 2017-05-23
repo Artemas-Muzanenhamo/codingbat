905b1cbaf772a7920f758d8f7596f3b0904cf4b1
amuzanenhamo-zaizi <amuzanenhamo@zaizi.com>
2017-05-23 11:17:56 +0100

    Completed the MapAB problem using the TDD approach

---
+++ src/main/java/map1/MapAB.java
@@ -0,0 +1,19 @@
+package map1;
+
+import java.util.Map;
+
+/**
+ * Created by amuzanenhamo on 23/05/2017.
+ */
+public class MapAB {
+    public static Map<String, String> mapAB(Map<String, String> map) {
+
+        if (map.containsKey("a") && map.containsKey("b")){
+
+            map.put("ab", map.get("a") + map.get("b"));
+        }
+
+        return map;
+
+    }
+}
---
+++ src/test/java/map1/MapABTest.java
@@ -0,0 +1,68 @@
+package map1;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by amuzanenhamo on 23/05/2017.
+ *
+ * Modify and return the given map as follows: for this problem the map may or may not contain the "a" and "b" keys.
+ * If both keys are present, append their 2 string values together and store the result under the key "ab".
+ *
+ * mapAB({"a": "Hi", "b": "There"}) → {"a": "Hi", "ab": "HiThere", "b": "There"}
+ * mapAB({"a": "Hi"}) → {"a": "Hi"}
+ * mapAB({"b": "There"}) → {"b": "There"}
+ */
+public class MapABTest {
+
+    Map<String, String> actualMap = new HashMap<>();
+    Map<String, String> expectedMap = new HashMap<>();
+
+    @Test
+    public void whenAnEmptyMapIsPassedThenReturnAnEmptyMap(){
+
+        assertTrue(MapAB.mapAB(actualMap).isEmpty());
+
+    }
+
+    @Test
+    public void whenMapWithKeyAIsPassedThenReturnMapAsIs(){
+
+        actualMap.put("a", "hello");
+
+        expectedMap.put("a", "hello");
+
+        assertEquals(expectedMap, MapAB.mapAB(actualMap));
+
+    }
+
+    @Test
+    public void whenMapWithKeyBIsPassedThenReturnMapAsIs(){
+
+        actualMap.put("b", "world");
+
+        expectedMap.put("b", "world");
+
+        assertEquals(expectedMap, MapAB.mapAB(actualMap));
+
+    }
+
+    @Test
+    public void whenMapWithBothKeysAPlusBThenReturnMapWithAKeyABPlusTheNewConcatenatedValue(){
+
+        actualMap.put("a", "hello");
+        actualMap.put("b", "world");
+
+        expectedMap.put("a", "hello");
+        expectedMap.put("b", "world");
+        expectedMap.put("ab", "helloworld");
+
+        assertEquals(expectedMap, MapAB.mapAB(actualMap));
+    }
+
+}
