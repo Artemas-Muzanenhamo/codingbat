b57a7ee6bde57992c2ea9b2bc24d6b2bde1d5323
amuzanenhamo-zaizi <amuzanenhamo@zaizi.com>
2017-05-26 10:18:56 +0100

    added some comments in Double23

--- src/main/java/array1/Double23.java
+++ src/main/java/array1/Double23.java
@@ -9,7 +9,7 @@ public class Double23 {
         if (nums.length > 0){
             if (nums.length == 2){
 
-                if (nums[0] == 2 || nums[0] == 3){
+                if (nums[0] == 2 || nums[0] == 3){ //can do better than this lol....
                     if (nums[0] == nums[1]){
                         return true;
                     }
