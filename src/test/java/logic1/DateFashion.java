fb8169715de4fa6f38dc94b1729741ca4d142c66
Artemas <artemasseng@gmail.com>
2017-06-23 23:33:17 +0100

    added a travis.yml file...

---
+++ src/test/java/logic1/DateFashion.java
@@ -0,0 +1,33 @@
+package logic1;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Artemas on 23/06/2017.
+ *
+ * You and your date are trying to get a table at a restaurant.
+ *
+ * The parameter "you" is the stylishness of your clothes, in the range 0..10, and "date" is the stylishness of your date's clothes.
+ *
+ * The result getting the table is encoded as an int value with 0=no, 1=maybe, 2=yes.
+ *
+ * If either of you is very stylish, 8 or more, then the result is 2 (yes).
+ *
+ * With the exception that if either of you has style of 2 or less, then the result is 0 (no).
+ *
+ * Otherwise the result is 1 (maybe).
+ *
+ * dateFashion(5, 10) → 2
+ * dateFashion(5, 2) → 0
+ * dateFashion(5, 5) → 1
+ */
+public class DateFashion {
+
+    @Test
+    public void whenYouOrYourDateAre8OrMoreThenReturn2(){
+        assertEquals();
+    }
+
+}
