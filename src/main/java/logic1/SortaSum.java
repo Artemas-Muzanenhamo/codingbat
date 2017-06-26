6361673b4ee21ee9caeae0536ebd7c892b145948
amuzanenhamo <artemasm@miless33.co.uk>
2017-06-26 14:21:10 +0100

    Completed SortaSum using the TDD approach

---
+++ src/main/java/logic1/SortaSum.java
@@ -0,0 +1,15 @@
+package logic1;
+
+/**
+ * Created by artemasm on 26/06/2017.
+ */
+public class SortaSum {
+    public static int sortaSum(int a, int b) {
+
+        if ( (a + b) < 10 || (a + b) > 19){
+            return a + b;
+        }
+
+        return 20;
+    }
+}
---
+++ src/test/java/logic1/SortaSumTest.java
@@ -0,0 +1,34 @@
+package logic1;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by artemasm on 26/06/2017.
+ *
+ * Given 2 ints, a and b, return their sum.
+ *
+ * However, sums in the range 10..19 inclusive, are forbidden, so in that case just return 20.
+ *
+ * sortaSum(3, 4) → 7
+ * sortaSum(9, 4) → 20
+ * sortaSum(10, 11) → 21
+ */
+public class SortaSumTest {
+
+    @Test
+    public void whenSumOfIntegersPassedInIsNotInTheRangeOf10To19ThenReturnSumValue(){
+        assertEquals(9, SortaSum.sortaSum(5, 4));
+    }
+
+    @Test
+    public void whenSumOfIntegersPassedInIsInTheRangeOf10To19ThenReturn20(){
+        assertEquals(20, SortaSum.sortaSum(9, 4));
+    }
+
+    @Test
+    public void whenSumOfIntegersPassedInIsAboveTheRangeOf10To19ThenReturnTheSumValue(){
+        assertEquals(40, SortaSum.sortaSum(20, 20));
+    }
+}
