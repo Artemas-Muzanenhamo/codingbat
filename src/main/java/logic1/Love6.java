04785b0635586bc21a9cb336cf72463acc750187
amuzanenhamo <artemasm@miless33.co.uk>
2017-06-27 12:44:52 +0100

    Completed Love6 using the TDD approach...

---
+++ src/main/java/logic1/Love6.java
@@ -0,0 +1,23 @@
+package logic1;
+
+/**
+ * Created by artemasm on 27/06/2017.
+ */
+public class Love6 {
+    public static boolean love6(int a, int b) {
+
+        if (a == 6 || b == 6){
+            return true;
+        }
+
+        if ((a + b) == 6){
+            return true;
+        }
+
+        if ( (Math.abs(a - b)) == 6 ){
+            return true;
+        }
+
+        return false;
+    }
+}
---
+++ src/test/java/logic1/Love6Test.java
@@ -0,0 +1,43 @@
+package logic1;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertNotNull;
+import static junit.framework.TestCase.assertTrue;
+
+/**
+ * Created by artemasm on 27/06/2017.
+ *
+ * The number 6 is a truly great number.
+ *
+ * Given two int values, a and b, return true if either one is 6. Or if their sum or difference is 6.
+ *
+ * Note: the function Math.abs(num) computes the absolute value of a number.
+ *
+ * love6(6, 4) → true
+ * love6(4, 5) → false
+ * love6(1, 5) → true
+ */
+public class Love6Test {
+
+    @Test
+    public void whenOneValueHasTheValue6ThenReturnTrue(){
+        assertTrue(Love6.love6(6, 4));
+        // lazy
+        assertTrue(Love6.love6(4, 6));
+    }
+
+    @Test
+    public void whenSumOfTheTwoValuesAddsUpTo6ThenReturnTrue(){
+        assertTrue(Love6.love6(1, 5));
+        // lazy
+        assertTrue(Love6.love6(5, 1));
+    }
+
+    @Test
+    public void whenTheDifferenceOfTheTwoValuesAddsUpTo6ThenReturnTrue(){
+        assertTrue(Love6.love6(1, 7));
+        // lazy
+        assertTrue(Love6.love6(8, 2));
+    }
+}
