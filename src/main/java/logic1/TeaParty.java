a03b63fd3927019a9360ae562ab06ee642714d51
amuzanenhamo <artemasm@miless33.co.uk>
2017-06-28 17:21:58 +0100

    Completed TeaParty using the TDD approach.

---
+++ src/main/java/logic1/TeaParty.java
@@ -0,0 +1,24 @@
+package logic1;
+
+/**
+ * Created by artemasm on 28/06/2017.
+ */
+public class TeaParty {
+    public static int teaParty(int tea, int candy) {
+
+        if (tea < 5 || candy < 5){
+            return 0;
+        }
+
+        if (tea >= 5 || candy >= 5){
+            if (tea >= candy * 2 || candy >= tea * 2){
+                return 2;
+            }else {
+                return 1;
+            }
+        }
+
+        return 0;
+
+    }
+}
---
+++ src/test/java/logic1/TeaPartyTest.java
@@ -0,0 +1,40 @@
+package logic1;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+/**
+ * Created by artemasm on 28/06/2017.#
+ *
+ * We are having a party with amounts of tea and candy.
+ * Return the int outcome of the party encoded as 0=bad, 1=good, or 2=great.
+ *
+ * A party is good (1) if both tea and candy are at least 5.
+ *
+ * However, if either tea or candy is at least double the amount of the other one, the party is great (2).
+ *
+ * However, in all cases, if either tea or candy is less than 5, the party is always bad (0).
+ *
+ * teaParty(6, 8) → 1
+ * teaParty(3, 8) → 0
+ * teaParty(20, 6) → 2
+ */
+public class TeaPartyTest {
+
+    @Test
+    public void whenEitherTeaOrCandyAreBelow5ThenReturn0(){
+        assertEquals(0, TeaParty.teaParty(3, 8));
+    }
+
+    @Test
+    public void whenCandyAndTeaAreGreaterThan5ThenReturn1(){
+        assertEquals(1, TeaParty.teaParty(5, 9));
+    }
+
+    @Test
+    public void whenCandyIsDoubleTheAmountOfTeaThenReturn2(){
+        assertEquals(2, TeaParty.teaParty(5, 10));
+    }
+}
