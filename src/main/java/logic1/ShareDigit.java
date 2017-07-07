85e54509968456e802be95ce8f1b6ba194be6492
Artemas <artemasseng@gmail.com>
2017-07-07 14:27:23 +0100

    Completed ShareDigit using the TDD approach...

---
+++ src/main/java/logic1/ShareDigit.java
@@ -0,0 +1,21 @@
+package logic1;
+
+/**
+ * Created by Artemas on 07/07/2017.
+ */
+public class ShareDigit {
+    public static boolean shareDigit(int a, int b) {
+
+        int aFirstIndex = a/10;
+        int aSecIndex = a%10;
+
+        int bFirstIndex = b/10;
+        int bSecIndex = b%10;
+
+        if (aFirstIndex == bFirstIndex || aFirstIndex == bSecIndex || aSecIndex == bFirstIndex || aSecIndex == bSecIndex){
+            return true;
+        }
+
+        return false;
+    }
+}
---
+++ src/test/java/logic1/ShareDigitTest.java
@@ -0,0 +1,30 @@
+package logic1;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by Artemas on 07/07/2017.
+ *
+ * Given two ints, each in the range 10..99, return true if there is a digit that appears in both numbers, such as the 2 in 12 and 23.
+ *
+ * (Note: division, e.g. n/10, gives the left digit while the % "mod" n%10 gives the right digit.)
+ *
+ * shareDigit(12, 23) → true
+ * shareDigit(12, 43) → false
+ * shareDigit(12, 44) → false
+ */
+public class ShareDigitTest {
+
+    @Test
+    public void whenValuesPassedInHaveNumbersThatAppearInBothValuesTheReturnTrue(){
+        assertTrue(ShareDigit.shareDigit(12, 23));
+    }
+
+    @Test
+    public void whenValuesPassedInDoNotHaveNumbersThatAppearInBothValuesTheReturnFalse(){
+        assertFalse(ShareDigit.shareDigit(12, 44));
+    }
+}
