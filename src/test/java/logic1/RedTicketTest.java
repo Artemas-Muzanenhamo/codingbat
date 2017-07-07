dff3c013ef4431c682ecc51a2df82a11617fad18
Artemas <artemasseng@gmail.com>
2017-07-07 12:31:03 +0100

    Completed RedTicket using the TDD approach

---
+++ src/test/java/logic1/RedTicketTest.java
@@ -0,0 +1,48 @@
+package logic1;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Artemas on 07/07/2017.
+ *
+ * You have a red lottery ticket showing ints a, b, and c, each of which is 0, 1, or 2.
+ *
+ * If they are all the value 2, the result is 10.
+ *
+ * Otherwise if they are all the same, the result is 5.
+ *
+ * Otherwise so long as both b and c are different from a, the result is 1.
+ *
+ * Otherwise the result is 0.
+ *
+ * redTicket(2, 2, 2) → 10
+ * redTicket(2, 2, 1) → 0
+ * redTicket(0, 0, 0) → 5
+ */
+public class RedTicketTest {
+
+    @Test
+    public void whenAllTheValuesAre2ThenReturn10(){
+        assertEquals(10, RedTicket.redTicket(2, 2, 2));
+    }
+
+    @Test
+    public void whenAllTheValuesAreTheSameButNot2_2_2ThenReturn5(){
+        assertEquals(5, RedTicket.redTicket(3, 3, 3));
+    }
+
+    @Test
+    public void whenBothB_And_C_AreDifferentFromAThenReturn1(){
+        assertEquals(1, RedTicket.redTicket(1, 2, 2));
+
+        assertEquals(1, RedTicket.redTicket(1, 2, 0));
+    }
+
+    @Test
+    public void whenAllTheValuesAreDifferentFromEachOtherThenReturn0(){
+
+        assertEquals(0, RedTicket.redTicket(2, 2, 1));
+    }
+}
