7c5f0ed6132460d189ee7081441c24fb25b3e0e8
amuzanenhamo <artemasm@miless33.co.uk>
2017-07-28 17:38:38 +0100

    Completed Math1 using the TDD approach

---
+++ src/main/java/functional1/Math1.java
@@ -0,0 +1,15 @@
+package functional1;
+
+import java.util.List;
+
+/**
+ * Created by artemasm on 28/07/2017.
+ */
+public class Math1 {
+    public static List<Integer> math(List<Integer> nums) {
+
+        nums.replaceAll(n -> (n + 1) * 10 );
+        return nums;
+
+    }
+}
---
+++ src/test/java/functional1/Math1Test.java
@@ -0,0 +1,53 @@
+package functional1;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static junit.framework.TestCase.assertEquals;
+
+/**
+ * Created by artemasm on 28/07/2017.
+ *
+ * Given a list of integers, return a list where each integer is added to 1 and the result is multiplied by 10.
+ *
+ * math1([1, 2, 3]) → [20, 30, 40]
+ * math1([6, 8, 6, 8, 1]) → [70, 90, 70, 90, 20]
+ * math1([10]) → [110]
+ */
+public class Math1Test {
+
+    List<Integer> actualNum;
+    List<Integer> expectedNum;
+
+    @Test
+    public void whenANumberInAListIsPassedThenReturnThenNumberAddedTo_1_And_MultipliedBy10(){
+
+        actualNum = new ArrayList<>();
+        actualNum.add(1);
+
+        expectedNum = new ArrayList<>();
+        expectedNum.add(20);
+
+        assertEquals(expectedNum, Math1.math(actualNum));
+
+    }
+
+    @Test
+    public void whenAListIsPassedThenReturnThenNumbersAddedTo_1_And_MultipliedBy10(){
+
+        actualNum = new ArrayList<>();
+        actualNum.add(1);
+        actualNum.add(2);
+        actualNum.add(3);
+
+        expectedNum = new ArrayList<>();
+        expectedNum.add(20);
+        expectedNum.add(30);
+        expectedNum.add(40);
+
+        assertEquals(expectedNum, Math1.math(actualNum));
+
+    }
+}
