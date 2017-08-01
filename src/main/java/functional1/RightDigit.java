8f3ddf19da326a4de873443e5f217ff844a92abc
amuzanenhamo <artemasm@miless33.co.uk>
2017-08-01 11:52:51 +0100

    Completed RightDigit using the TDD approach.

---
+++ src/main/java/functional1/RightDigit.java
@@ -0,0 +1,15 @@
+package functional1;
+
+import java.util.List;
+
+/**
+ * Created by artemasm on 01/08/2017.
+ */
+public class RightDigit {
+    public static List<Integer> rightDigit(List<Integer> nums) {
+
+        nums.replaceAll(n -> n % 10);
+        return nums;
+
+    }
+}
---
+++ src/test/java/functional1/RightDigitTest.java
@@ -0,0 +1,59 @@
+package functional1;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static junit.framework.TestCase.assertEquals;
+
+/**
+ * Created by artemasm on 01/08/2017.
+ *
+ * Given a list of non-negative integers, return an integer list of the rightmost digits. (Note: use %)
+ *
+ * rightDigit([1, 22, 93]) → [1, 2, 3]
+ * rightDigit([16, 8, 886, 8, 1]) → [6, 8, 6, 8, 1]
+ * rightDigit([10, 0]) → [0, 0]
+ *
+ */
+public class RightDigitTest {
+
+    List<Integer> actualNumber;
+    List<Integer> expectedNumber;
+
+    @Test
+    public void whenANumberInAListIsPassedThenReturnTheRightMostDigit(){
+
+        actualNumber = new ArrayList<>();
+        actualNumber.add(12);
+
+        expectedNumber = new ArrayList<>();
+        expectedNumber.add(2);
+
+        assertEquals(expectedNumber, RightDigit.rightDigit(actualNumber));
+
+    }
+
+    @Test
+    public void whenNumbersInAListArePassedInThenReturnTheRightMostDigits(){
+
+        actualNumber = new ArrayList<>();
+        actualNumber.add(16);
+        actualNumber.add(8);
+        actualNumber.add(886);
+        actualNumber.add(10);
+        actualNumber.add(1);
+
+        expectedNumber = new ArrayList<>();
+        expectedNumber.add(6);
+        expectedNumber.add(8);
+        expectedNumber.add(6);
+        expectedNumber.add(0);
+        expectedNumber.add(1);
+
+        assertEquals(expectedNumber, RightDigit.rightDigit(actualNumber));
+
+    }
+
+}
