9d93b43efd5f4fc4e0b167b193b0827858c68c66
Artemas <artemasseng@gmail.com>
2017-09-12 11:05:12 +0100

    Completed StartWord using the TDD approach.

---
+++ src/main/java/string1/StartWord.java
@@ -0,0 +1,25 @@
+package string1;
+
+/**
+ * Created by Artemas on 12/09/2017.
+ */
+public class StartWord {
+    public String startWord(String string, String word) {
+
+        if (word.length() > string.length()) {
+            return "";
+        }
+
+        if (string.substring(0, word.length()).equals(word)) {
+            return word;
+        }
+
+        if (string.substring(1, word.length()).equals(word.substring(1))){
+            return string.charAt(0) + word.substring(1);
+        }
+
+
+        return "";
+
+    }
+}
--- src/test/java/string1/StartWordTest.java
+++ src/test/java/string1/StartWordTest.java
@@ -1,10 +1,19 @@
 package string1;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
 /**
  * Created by amuzanenhamo on 05/05/2017.
  *
  * Given a string and a second "word" string, we'll say that the word matches the string if it appears at the front of the string,
- * except its first char does not need to match exactly. On a match, return the front of the string, or otherwise return the empty string.
+ *
+ * except its first char does not need to match exactly.
+ *
+ * On a match, return the front of the string, or otherwise return the empty string.
+ *
  * So, so with the string "hippo" the word "hi" returns "hi" and "xip" returns "hip". The word will be at least length 1.
  *
  * startWord("hippo", "hi") → "hi"
@@ -12,4 +21,27 @@ package string1;
  * startWord("hippo", "i") → "h"
  */
 public class StartWordTest {
+
+    private StartWord startWord;
+
+    @Before
+    public void init(){
+        startWord = new StartWord();
+    }
+
+    @Test
+    public void whenAnEmptyStringAndAnEmptyWordArePassedThenReturnNothing(){
+        assertEquals("", startWord.startWord("", ""));
+    }
+
+    @Test
+    public void whenAWordIsLength1ThenReturnTheStringFirstChar(){
+        assertEquals("h", startWord.startWord("hippo", "i"));
+    }
+
+    @Test
+    public void whenAWordPassedHasMatchingCharsInTheStringThenReturnAStringMadeOfBoth(){
+        assertEquals("hip", startWord.startWord("hippo", "xip"));
+    }
+
 }
