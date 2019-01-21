ac7e99b1e49e3fbee1aef3e0dc812d9713083307
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2019-01-21 08:38:05 +0000

    amend Program2Test

--- src/main/java/warmup1/Program2.java
+++ src/main/java/warmup1/Program2.java
@@ -1,21 +1,17 @@
 package warmup1;
 
-public class Program2 {
-	
-	public boolean loneTeen(int a, int b){
-		
-		//if (a || b are teen) return true
-		//if (a && b are not teen) return false
-		
-		if ((a >=13 && a <=19 ) && (b >=13 && b <=19)){
-			//13, 13
-			//99, 99
-			return false;
-		}else if((a < 13 || a > 19) && (b < 13 || b > 19)){
-			return false;
-		}else{
-			return true;
-		}
-	}
+class Program2 {
+
+    boolean loneTeen(int a, int b) {
+
+        //if (a || b are teen) return true
+        //if (a && b are not teen) return false
+
+        if ((a >= 13 && a <= 19) && (b >= 13 && b <= 19)) {
+            //13, 13
+            //99, 99
+            return false;
+        } else return (a >= 13 && a <= 19) || (b >= 13 && b <= 19);
+    }
 
 }
